0e1975dc462acc29634999fbf7c508516abf713e
Larry Petroski <larry@Petro.local>
2019-05-26 16:40:27 -0500

    added logic if no beacons logged

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
 
 import Details from './Details'
 import Chart from './Chart'
+import selectBeacons from '../selectors/beacons'
 
-const Dashboard = () => (
-    <div>
-        <Details />
-        <Chart />
-    </div>
-)
+const styles = theme => ({
+    noData: {
+        marginTop: '15px',
+    },
+})
+const Dashboard = (props) => {
+    const { classes } = props
+    return (
+        <div>
+            {props.beacons.length === 0 ? (
+                <div>
+                    <Typography variant='h4' align='center' color='inherit' className={classes.noData}>
+                        No Data Logged
+                </Typography>
+                </div>
+            ) : (<div>
+                <Details />
+                <Chart />
+            </div>)}
+        </div>
+    )
+}
 
-export default Dashboard
+const mapStateToProps = (state) => {
+    return {
+        beacons: selectBeacons(state)
+    }
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Dashboard))
